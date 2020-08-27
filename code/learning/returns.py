c57ac4c91c489e2a3f50fd78c3571932584e521a
vickykatoch <vickykatoch@yahoo.com>
2020-08-26 20:38:42 -0400

    changes

--- code/learning/returns.py
+++ code/learning/returns.py
@@ -2,4 +2,9 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 
-print('Reached here successfully')
+# Download APPL Stock price 
+df = yf.download('AAPL', start='1999-12-11', end='2010-12-31', progress=False)
+df.head(10)
+
+
+# print('Reached here successfully')
