5b7db572a75f4d3cdbf8a98646cb372a24f9ed96
vickykatoch <vickykatoch@yahoo.com>
2020-09-09 10:24:03 -0400

    changes

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 *http://csrgxtu.github.io/2015/03/20/Writing-Mathematic-Fomulars-in-Markdown/*
 
 https://financetrain.com/multivariate-linear-regression-in-python-with-scikit-learn-library/#:~:text=%20Multivariate%20Linear%20Regression%20in%20Python%20with%20scikit-learn,training%20the%20dataset%2C%20we%20will%20make...%20More%20
+C:/ProgramData/Anaconda3/Scripts/activate.bat
--- code/learning/returns.py
+++ code/learning/returns.py
@@ -29,10 +29,10 @@ df['log_rtn'] = np.log(df.adj_close/df.adj_close.shift(1))
 # print(df.head(5))
 
 # Download Consumer Price Index from Quandl
-df_cpi = quandl.get(dataset='RATEINF/CPI_USA', start_date='1999-12-01',  end_date='2010-12-31')
+df_cpi = quandl.get(dataset='RATEINF/CPI_USA', start_date='1999-12-31',  end_date='2010-12-31')
 df_cpi.rename(columns= {'Value' : 'cpi'}, inplace=True)
 
-df_dates = pd.DataFrame(index=pd.date_range(start='1999-12-01',  end='2010-12-31'))
+df_dates = pd.DataFrame(index=pd.date_range(start='1999-12-31',  end='2010-12-31'))
 
 print(df_dates.asfreq('M').head(20))
 
